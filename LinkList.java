5688dee9e2f4e7dd22e22e60c84328488e888dea
Jagadish Hegade <42802228+J3-Aquacool@users.noreply.github.com>
2024-10-11 14:30:10 +0530

    Add files via upload

---
+++ LinkList.java
@@ -0,0 +1,14 @@
+LinkedList<String> list = new LinkedList<String>();
+		  list.add("Boolean");
+	           list.add("FileInputStream");
+	           list.addLast("System");
+	           list.addFirst("ArrayList");
+		  System.out.println ("Original contents of list: " + list);
+	           list.add(1, "Array");
+	           System.out.println("Contents of list: " + list);
+	           list.remove("FileInputStream");
+	           list.remove(2);
+	           System.out.println ("Contents after deletion: " + list);
+	           list.removeFirst();
+	           list.removeLast();
+	           System.out.println ("After deleting first & last:" + list);
